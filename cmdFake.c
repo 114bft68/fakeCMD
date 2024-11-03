212bf16d7c05c8cf12f289826c68b1a9ad8ed93f
114bft68 <168547332+114bft68@users.noreply.github.com>
2024-11-03 18:13:07 +0800

    useless

--- cmdFake.c
+++ cmdFake.c
@@ -3,12 +3,31 @@
 #include <string.h>
 #include <errno.h>
 
-const char* COMMANDS[] = {"--help", "--sendhello", "--fileswap", "--ttt", "--exit"};
+#define freealloc(...) do { \
+    void* tobefreed[] = {__VA_ARGS__}; \
+    for (int i = 0; i < sizeof(tobefreed) / sizeof(tobefreed[0]); i++) { \
+        free(tobefreed[i]); \
+        tobefreed[i] = NULL; \
+    } \
+} while (0)
 
-void send(char* store, int true_false);
+#define clearBuffer(variable, lengthOfInput) do { \
+    if (strlen(variable) == (lengthOfInput - 1) && variable[lengthOfInput - 2] != '\n') { /* strlen ignores \0 */ \
+        int c; \
+        while ((c = getchar()) != '\n' && c != EOF) {} \
+    } \
+} while (0)
+
+const char* COMMANDS[] = {"--help", "--sendhello", "--swap2textfiles", "--tictactoe", "--clear", "--exit"};
+
+int commandsCount = sizeof(COMMANDS) / sizeof(COMMANDS[0]);
+
+void send(char* cmd, int true_false);
+
+void fileSwap(char* cmd);
 
 int commandIndex(char* compare) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < commandsCount; i++) {
         if (strcmp(compare, COMMANDS[i]) == 0) {
             return i;
         }
@@ -17,7 +36,7 @@ int commandIndex(char* compare) {
 }
 
 int main() {
-    char *command = malloc(21 * sizeof(char));
+    char *command = malloc(22 * sizeof(char));
 
     if (command == NULL) {
         printf("Memory allocation failed");
@@ -27,121 +46,137 @@ int main() {
     printf("===== Fake CMD =====\n");
     send(command, 1);
 
-    free(command);
+    freealloc(command);
     return 0;
 }
 
-void send(char* store, int true_false) {
+void send(char* cmd, int true_false) {
     if (true_false) {
-        printf("\nAvailable commands:\n--help\n--sendhello\n--fileswap\n--ttt\n--exit");
+        printf("\nAvailable commands:\n");
+        for (int i = 0; i < commandsCount; i++) {
+            printf("%s\n", COMMANDS[i]);
+        }
     }
-    printf("\n\n> ");
+    printf("\n> ");
     
-    fgets(store, 21, stdin);
+    fgets(cmd, 22, stdin); // 20 characters + \n and \0
 
-    if (strlen(store) == 20 && store[19] != '\n') {
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF) {}
-    }
+    clearBuffer(cmd, 22);
 
-    store[strcspn(store, "\n")] = '\0';
+    cmd[strcspn(cmd, "\n")] = '\0';
 
-    switch(commandIndex(store)) {
+    switch(commandIndex(cmd)) {
         case 0:
-            send(store, 1);
+            send(cmd, 1);
             break;
         case 1:
-            printf("\nHello");
-            send(store, 0);
+            printf("\nHello\n");
+            send(cmd, 0);
             break;
         case 2:
-            char* path1 = malloc(101 * sizeof(char));
-            char* path2 = malloc(101 * sizeof(char));
-            if (path1 == NULL || path2 == NULL) {
-                perror("\nAn error has occurred:");
-                send(store, 0);
-            }
-
-            FILE *file1, *file2;
-            
-            printf("\nPlease Input the First File Path\n>> ");
-            fgets(path1, 101, stdin);
-            printf("\nPlease Input the Second File Path\n>> ");
-            fgets(path2, 101, stdin);
-
-            path1[strcspn(path1, "\n")] = '\0';
-            path2[strcspn(path2, "\n")] = '\0';
-
-            if ((file1 = fopen(path1, "r")) == NULL || (file2 = fopen(path2, "r")) == NULL) {
-                if (file1) {
-                    fclose(file1);
-                }
-                free(path1);
-                free(path2);
-                perror("\nFailed to open file(s) for reading: ");
-                send(store, 0);
-            }
-
-            fseek(file1, 0, SEEK_END);
-            size_t allocated1 = ftell(file1);
-            char* file1Content = malloc(allocated1 + 1);
-            fseek(file1, 0, SEEK_SET);
-
-            fseek(file2, 0, SEEK_END);
-            size_t allocated2 = ftell(file2);
-            char* file2Content = malloc(allocated2 + 1);
-            fseek(file2, 0, SEEK_SET);
-
-            if (file1Content == NULL || file2Content == NULL) {
-                free(file1Content);
-                free(file2Content);
-                perror("\nAn error has occurred:");
-                send(store, 0);
-            }
-
-            size_t bytesRead1 = fread(file1Content, sizeof(char), allocated1 / sizeof(char), file1);
-            size_t bytesRead2 = fread(file2Content, sizeof(char), allocated2 / sizeof(char), file2);
-            file1Content[bytesRead1] = '\0';
-            file2Content[bytesRead2] = '\0';
-
-            fclose(file1);
-            fclose(file2);
-
-            if ((file1 = fopen(path1, "w")) == NULL || (file2 = fopen(path2, "w")) == NULL) {
-                if (file1) {
-                    fclose(file1);
-                }
-                free(file1Content);
-                free(file2Content);
-                perror("\nFailed to open file(s) for writing:");
-                send(store, 0);
-            }
-
-            free(path1);
-            free(path2);
-
-            if (fputs(file2Content, file1) == EOF || fputs(file1Content, file2) == EOF) {
-                perror("\nAn error has occurred:");
-            } else {
-                printf("\nSuccess!");
-            }
-
-            free(file1Content);
-            free(file2Content);
-
-            fclose(file1);
-            fclose(file2);
-
-            send(store, 0);
+            fileSwap(cmd);
             break;
         case 3:
-            printf("\nnot yet");
-            send(store, 0);
+            printf("\nnot yet\n");
+            send(cmd, 0);
             break;
         case 4:
-            exit(EXIT_SUCCESS);
+            #if defined(_WIN32)
+                system("cls");
+            #elif defined(__unix__)
+                system("clear");
+            #endif
+            printf("===== Fake CMD =====\n");
+            send(cmd, 0);
+            break;
+        case 5:
+            break;
         default:
-            send(store, 0);
+            send(cmd, 0);
             break;
     }
+}
+
+void fileSwap(char* cmd) {
+    char* path1 = malloc(102 * sizeof(char));
+    char* path2 = malloc(102 * sizeof(char)); // 100 characters + \n and \0
+    if (path1 == NULL || path2 == NULL) {
+        freealloc(path1, path2);
+        perror("\nAn error has occurred");
+        return send(cmd, 0);
+    }
+
+    FILE *file1, *file2;
+            
+    printf("\nPlease Input the First File Path\n>> ");
+    fgets(path1, 102, stdin);
+    clearBuffer(path1, 102);
+    printf("\nPlease Input the Second File Path\n>> ");
+    fgets(path2, 102, stdin);
+    clearBuffer(path2, 102);
+
+    path1[strcspn(path1, "\n")] = '\0';
+    path2[strcspn(path2, "\n")] = '\0';
+
+    if ((file1 = fopen(path1, "rb")) == NULL || (file2 = fopen(path2, "rb")) == NULL) {
+        if (file1) {
+            fclose(file1);
+        }
+        freealloc(path1, path2);
+        perror("\nFailed to open file(s) for reading");
+        return send(cmd, 0);
+    }
+
+    fseek(file1, 0, SEEK_END);
+    long allocated1 = ftell(file1);
+    char* file1Content = malloc(allocated1);
+    fseek(file1, 0, SEEK_SET);
+
+    fseek(file2, 0, SEEK_END);
+    long allocated2 = ftell(file2);
+    char* file2Content = malloc(allocated2);
+    fseek(file2, 0, SEEK_SET);
+
+    if (file1Content == NULL || file2Content == NULL) {
+        freealloc(file1Content, file2Content, path1, path2);
+        fclose(file1);
+        fclose(file2);
+        perror("\nAn error has occurred");
+        return send(cmd, 0);
+    }
+
+    size_t charsRead1 = fread(file1Content, sizeof(char), allocated1 / sizeof(char), file1);
+    size_t charsRead2 = fread(file2Content, sizeof(char), allocated2 / sizeof(char), file2);
+
+    if (charsRead1 != allocated1 / sizeof(char) || charsRead2 != allocated2 / sizeof(char)) {
+        freealloc(file1Content, file2Content, path1, path2);
+        fclose(file1);
+        fclose(file2);
+        perror("\nAn error has occurred");
+        return send(cmd, 0);
+    }
+
+    if (freopen(path1, "wb", file1) == NULL || freopen(path2, "wb", file2) == NULL) {
+        if (file1) {
+            fclose(file1);
+        }
+        freealloc(file1Content, file2Content, path1, path2);
+        perror("\nFailed to open file(s) for writing");
+        return send(cmd, 0);
+    }
+
+    freealloc(path1, path2);
+
+    if (fwrite(file2Content, sizeof(char), charsRead2, file1) != charsRead2 || fwrite(file1Content, sizeof(char), charsRead1, file2) != charsRead1) {
+        perror("\nAn error has occurred");
+    } else {
+        printf("\nSuccess!\n");
+    }
+
+    freealloc(file1Content, file2Content);
+
+    fclose(file1);
+    fclose(file2);
+
+    send(cmd, 0);
 }
